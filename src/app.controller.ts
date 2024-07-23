800b96717909cb54f724aae6726597ce0e8ee6f6
leopham <hongthaipro@gmail.com>
2024-07-23 14:12:57 +0700

    setup action header

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -1,9 +1,17 @@
-import { Controller, Get } from '@nestjs/common';
+import { Controller, Get, UseInterceptors } from '@nestjs/common';
 import { AppService } from './app.service';
 
+import { ActionHeaderInterceptor } from './interceptors/action-header/action-header.interceptor';
+
 @Controller()
+@UseInterceptors(ActionHeaderInterceptor)
 export class AppController {
   constructor(private readonly appService: AppService) {}
+
+  @Get('/actions.json')
+  getActions() {
+    return this.appService.getRules();
+  }
 
   @Get()
   getHello(): string {
